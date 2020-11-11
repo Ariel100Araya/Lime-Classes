c49039c036f14b7465a3cb64a69e1ba71feb3b71
Ariel Araya <ariel100araya@outlook.com>
2020-11-11 17:46:12 -0500

    Can someone solve the (Build1.js not working on the TestView.html?

---
+++ Build1.js
@@ -0,0 +1,14 @@
+function myFunction() {
+    var docName = "Test 1";
+    var question1 = "What is 5+3?";
+    var question2 = "What is 2x3?";
+    var question3 = "What is 1x2?";
+    var question4 = "What is your favorite color?";
+    var question5 = "What is your birthday?";
+    document.getElementById("docName").innerHTML = docName;
+    document.getElementById("question1").innerHTML = question1;
+    document.getElementById("question2").innerHTML = question2;
+    document.getElementById("question3").innerHTML = question3;
+    document.getElementById("question4").innerHTML = question4;
+    document.getElementById("question5").innerHTML = question5;
+}
